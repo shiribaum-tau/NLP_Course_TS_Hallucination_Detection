4be5d96c44a71d40a7b6c845daf93e1b2f50add2
HSR <romdrori@mail.tau.ac.il>
2024-10-14 17:15:59 +0300

    PRAISE ALLAH, finding the bad strings worksgit add *

--- labeler/hallucinations_labeler.py
+++ labeler/hallucinations_labeler.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 import numpy as np
+import re
 import matplotlib.pyplot as plt
 from transformers import GPTNeoXForCausalLM, AutoTokenizer
 
@@ -18,6 +19,8 @@ auxiliary_verbs = [
 
 
 punctuations_extended = r"""!"#$%&'()*+,-–./:;<=>?@[\]^_`{|}~"""
+
+PUNCT_EXTRA = "<PUNC>"
 
 
 def delete_word_from_atom_sentence_and_join(word: str, atom_sentence: list):
@@ -37,17 +40,17 @@ def remove_auxiliary_verbs(string: str):
 
 def reduce_facts(atoms, method):
     if len(atoms) == 1:
-        return [atoms[0]['text'].lower()], [atoms[0]['label']]
+        return [atoms[0]['text']], [atoms[0]['label']]
     cut_atoms = []
 
     if method == 'Weak Uniqueness':
-        unique_words = set(atoms[0]['text'].lower().split(' '))
+        unique_words = set(atoms[0]['text'].split(' '))
 
         for atom in atoms[1:]:
-            current_unique_words = set(atom['text'].lower().split(' '))
+            current_unique_words = set(atom['text'].split(' '))
             unique_words = unique_words.intersection(current_unique_words)
         for atom in atoms:
-            atom_sentence = atom['text'].lower()
+            atom_sentence = atom['text']
             for word in unique_words:
                 atom_sentence = atom_sentence.split(" ")
                 atom_sentence = delete_word_from_atom_sentence_and_join(word, atom_sentence)
@@ -58,7 +61,7 @@ def reduce_facts(atoms, method):
     if method == "Cascading Deletion":
         atoms_df = pd.DataFrame(atoms)['text']
         atoms_num = len(atoms_df)
-        atoms_df = [atoms_df[i].lower() for i in range(atoms_num)]
+        atoms_df = [atoms_df[i] for i in range(atoms_num)]
         labels = [pd.DataFrame(atoms)['label'][i] for i in range(atoms_num)]
 
         atoms_to_labels = {}
@@ -97,23 +100,14 @@ def split_string(string: str):
 
 def find_index_of_fact_in_sentence(fact, sentence):
     split_fact = split_string(fact)
-    split_sentence = split_string(sentence)
 
     ind_list = []
-    for i, fact_word in enumerate(split_fact):
-        if fact_word not in split_sentence:
-            continue
-        word_ind = split_sentence.index(fact_word)
-        ind_list.append(word_ind)
-    ind_array = np.array(ind_list)
-    n = len(ind_array)
-    # Given an exponentially decaying weight to the indices, to find the proper index where the fact leads.
-    # sum of all weight should be 1.
-    if n == 0:
-        return -1, None
-    a = (0.5/(1-(0.5)**n))
-    weights_array_exp = np.array([a/(2**i) for i in range(n-1, -1, -1)])
-    return int(ind_array @ weights_array_exp), ind_list
+    for fact_word in split_fact:
+        appearances_list = [(m.start(), m.end()) for m in re.finditer(rf'\b{fact_word}\b', sentence)]
+        for couple in appearances_list:
+            ind_list.append(couple)
+    return ind_list
+
 
 
 def read_our_json(filename):
@@ -154,33 +148,13 @@ def read_jsonl(filename):
     return df
 
 
-def find_pre_sentence(paragraph: str, index):
-    sub_paragraph = paragraph[:index]
-    # last_dot_index = sub_paragraph.rfind('.')
-    # last_colun_index = sub_paragraph.rfind(':')
-    # dot_semi_colun = paragraph[max(last_dot_index, last_colun_index) + 1: index]
-
-    if index == 0:
-        return "", -1
-    backwards_indices = 1
-    while sub_paragraph[-backwards_indices] in [" ", "\n"]:
-        backwards_indices += 1
-        if backwards_indices == len(sub_paragraph) + 1:
-            break
-    beginning_index = index - backwards_indices + 1
-    x = paragraph[beginning_index : index]
-    return x, beginning_index
-
-
 def get_full_sentence_start_and_end_index(partial_sentence, generation, previous_sentence_end_index):
-    sentence_text_beginning_ind = generation.find(partial_sentence, previous_sentence_end_index + 1)
-    if sentence_text_beginning_ind == -1:
+    beginning_ind = generation.find(partial_sentence, previous_sentence_end_index + 1)
+    if beginning_ind == -1:
         # Should never happen.
         raise Exception("FUUUUCCCCKKKKKK, I ASSUMED THE ENTIRE SENTENCE IS INSIDE THE ORIGINAL, FUCK")
-    pre_sentence, beginning_ind = find_pre_sentence(generation, sentence_text_beginning_ind)
-    full_sentence = f"{pre_sentence}{partial_sentence}"
-    end_index = previous_sentence_end_index + len(partial_sentence) - 1
-    return full_sentence, end_index, beginning_ind
+    end_index = beginning_ind + len(partial_sentence) - 1
+    return end_index, beginning_ind
 
 
 def rebuild_original_paragraph(so_far_rebuilt_paragraph, sentence_to_rebuild):
@@ -200,54 +174,80 @@ def find_hallucination_indices_in_sentence(facts, labels, sentence):
         # print(rrfact)
 
         if rrfact != "":
-            fact_ind, fact_indices_list = find_index_of_fact_in_sentence(rrfact, sentence)
-            if fact_ind == -1 or fact_indices_list is None:
+            fact_indices_couple_list = find_index_of_fact_in_sentence(rrfact, sentence)
+            if len(fact_indices_couple_list) == 0:
                 continue
-            for ind in fact_indices_list:
-                sentence_hallucinations_indices.append(ind)
+            for couple in fact_indices_couple_list:
+                sentence_hallucinations_indices.append(couple)
     return sentence_hallucinations_indices
 
 
+def replace_punctuation(text):
+    # Create a regex pattern that matches any punctuation character
+    pattern = f"[{re.escape(punctuations_extended)}]"
+    # Replace each punctuation mark with " {PUNCT_EXTRA}P{PUNCT_EXTRA} "
+    replaced_text = re.sub(pattern, f" {PUNCT_EXTRA}\g<0>{PUNCT_EXTRA} ", text)
+    return replaced_text
+
+
+def insert_letter(string, letter, index):
+    return string[:index] + letter + string[index:]
+
+
+def chatgpt_finds_couples(atomic_facts, sentence_text):
+    raise NotImplementedError("lol noob get not implemented")
+
+
+ROM_ALGO_BEST_ALGO = True
 show_print = True
 remove_punctuation_from_facts = True
-example_output_2 = r"C:\Users\Arik Drori\Desktop\Year3+\NLP\FinalProject\ts_hallucination\fact_checked_data\pythia_2.8_deterministic_fact_checked_bm25.json"
+example_output_2 = r"C:\Users\Arik Drori\Desktop\Year3+\NLP\FinalProject\ts_hallucination\fact_checked_data\pythia_2.8_deterministic_fact_checked_gtr.json"
 df = read_our_json(example_output_2)
 generations = df['output']
 annotations = df['annotations']
+
 for annotation, generation in zip(annotations, generations):
     if annotation is None:
         continue
-    total_annotation_length = 0
-    hallucination_indices = []
-    rebuilt_paragraph = []
+    hallucination_indices_couples = []
     last_sentence_end_index = -1
     for sentence in annotation:
-        sentence_text, this_sentence_end_ind, this_sentence_start_ind = get_full_sentence_start_and_end_index(sentence['text'], generation,
-                                                                                          last_sentence_end_index)
+        sentence_text = sentence['text']
+        last_sentence_end_index, this_sentence_start_ind = get_full_sentence_start_and_end_index(sentence_text, generation,
+                                                                                               last_sentence_end_index)
 
-        last_sentence_start_ind = this_sentence_end_ind
         relevancy = sentence['is-relevant']
         atomic_facts = sentence['human-atomic-facts']
-        rebuild_original_paragraph(rebuilt_paragraph, sentence_text)
 
         if atomic_facts is None or len(atomic_facts) == 0:
             continue
 
-        # Reduce facts to weak-uniqueness (Words that appear in ALL atoms are delteted), or Cascading Deletion (Every
-        # word seen in a fact will be deleted in the following ones)
-        reduced_facts, labels = reduce_facts(atomic_facts, method='Cascading Deletion')
-        sentence_hallucinations = find_hallucination_indices_in_sentence(reduced_facts, labels, sentence_text)
-        for ind in sentence_hallucinations:
-            hallucination_indices.append(ind + total_annotation_length)
-        total_annotation_length += len(split_string(sentence_text))
+        if ROM_ALGO_BEST_ALGO:
+            # Reduce facts to weak-uniqueness (Words that appear in ALL atoms are delteted), or Cascading Deletion (Every
+            # word seen in a fact will be deleted in the following ones)
+            # And then find where those hallucinataions are in the given sentence
+            reduced_facts, labels = reduce_facts(atomic_facts, method='Cascading Deletion')
+            sentence_hallucinations_couples = find_hallucination_indices_in_sentence(reduced_facts, labels, sentence_text)
+        else:
+            sentence_hallucinations_couples = chatgpt_finds_couples(atomic_facts, sentence_text)
+
+        for couple in sentence_hallucinations_couples:
+            s, e = couple
+            hallucination_indices_couples.append((s + this_sentence_start_ind, e + this_sentence_start_ind))
 
     if show_print:
-        for i in hallucination_indices:
-            rebuilt_paragraph[i] = f"[{rebuilt_paragraph[i]}]"
-        marked_generations = " ".join(rebuilt_paragraph)
-        print(marked_generations)
+        hallucination_indices_couples_from_end = sorted(list(set(hallucination_indices_couples)), key=lambda x: -x[1])
+        generation_to_change = generation
+        for couple in hallucination_indices_couples_from_end:
+            s, e = couple
+            generation_to_change = insert_letter(generation_to_change, "]", e)
+            generation_to_change = insert_letter(generation_to_change, "[", s)
+        print(generation_to_change)
         # what we want to return is hallucinations indices and sentence text tokenized => Only need to notice that /n are
         # cut in the whole ordeal.
+
+
+    
 
 
 #print(df)
